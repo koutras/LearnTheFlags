ac3a0b26cfbbb03d41e5b06742183a4bf9504a37
Apostolos Koutras <koutras.apostolos@gmail.com>
2015-02-20 00:11:45 +0200

    added the flags, some too big

--- app/src/main/java/com/example/akis/learntheflags/MainActivity.java
+++ app/src/main/java/com/example/akis/learntheflags/MainActivity.java
@@ -2,25 +2,146 @@ package com.example.akis.learntheflags;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private String isoFlags[]={
+            "AD", "AE", "AF", "AG", "AI", "AL", "AM", "AO", "AQ", "AR",
+            "AS", "AT", "AU", "AW", "AX", "AZ", "BA", "BB", "BD", "BE", "BF", "BG",
+            "BH", "BI", "BJ", "BL", "BM", "BN", "BO", "BQ", "BR", "BS", "BT", "BV",
+            "BW", "BY", "BZ", "CA", "CC", "CD", "CF", "CG", "CH", "CI", "CK", "CL",
+            "CM", "CN", "CO", "CR", "CU", "CV", "CW", "CX", "CY", "CZ", "DE", "DJ",
+            "DOM", "DK", "DM", "DZ", "EC", "EE", "EG", "EH", "ER", "ES", "ET", "FI",
+            "FJ", "FK", "FM", "FO", "FR", "GA", "GB", "GD", "GE", "GF", "GG", "GH",
+            "GI", "GL", "GM", "GN", "GP", "GQ", "GR", "GS", "GT", "GU", "GW", "GY",
+            "HK", "HM", "HN", "HR", "HT", "HU", "ID", "IE", "IL", "IM", "IN", "IO",
+            "IQ", "IR", "IS", "IT", "JE", "JM", "JO", "JP", "KE", "KG", "KH", "KI",
+            "KM", "KN", "KP", "KR", "KW", "KY", "KZ", "LA", "LB", "LC", "LI", "LK",
+            "LR", "LS", "LT", "LU", "LV", "LY", "MA", "MC", "MD", "ME", "MF", "MG",
+            "MH", "MK", "ML", "MM", "MN", "MO", "MP", "MQ", "MR", "MS", "MT", "MU",
+            "MV", "MW", "MX", "MY", "MZ", "NA", "NC", "NE", "NF", "NG", "NI", "NL",
+            "NO", "NP", "NR", "NU", "NZ", "OM", "PA", "PE", "PF", "PG", "PH", "PK",
+            "PL", "PM", "PN", "PR", "PS", "PT", "PW", "PY", "QA", "RE", "RO", "RS",
+            "RU", "RW", "SA", "SB", "SC", "SD", "SE", "SG", "SH", "SI", "SJ", "SK",
+            "SL", "SM", "SN", "SO", "SR", "ST", "SV", "SX", "SY", "SZ", "TC", "TD",
+            "TF", "TG", "TH", "TJ", "TK", "TL", "TM", "TN", "TO", "TR", "TT", "TV",
+            "TW", "TZ", "UA", "UG", "UM", "US", "UY", "UZ", "VA", "VC", "VE", "VG",
+            "VI", "VN", "VU", "WF", "WS", "YE", "YT", "ZA", "ZM", "ZW"};
+
     private Button mTrueButton;
     private Button mFalseButton;
+    //private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private Button button;
+    private ImageView image;
+    private ArrayList<String> flagsList;
+
+
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+            new TrueFalse(R.string.question_oceans, true),
+            new TrueFalse(R.string.question_mideast, false),
+            new TrueFalse(R.string.question_americas, true),
+            new TrueFalse(R.string.question_africa, false),
+            new TrueFalse(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
+
+
+    public ArrayList<String> initializeList(){
+        ArrayList<String> isoFlagsList = new ArrayList();
+        for(int i=0;i<isoFlags.length;i++){
+            isoFlagsList.add(isoFlags[i]);
+        }
+        /* shuffling the list */
+        long seed = System.nanoTime();
+        Collections.shuffle(isoFlagsList, new Random(seed));
+        return isoFlagsList;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTrueButton = (Button)findViewById(R.id.true_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        int question = mQuestionBank[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
 
-        mFalseButton = (Button)findViewById(R.id.false_button);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        flagsList=initializeList();
+
+
+
+        System.out.println(flagsList.get(0));
+
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+
+        image = (ImageView) findViewById(R.id.imageView1);
+
+        button = (Button) findViewById(R.id.btnChangeImage);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if (flagsList.isEmpty())
+                    flagsList=initializeList();
+
+                String flagIso =  flagsList.remove(0);
+                System.out.println(flagIso);
+
+                int id = getResources().getIdentifier(flagIso.toLowerCase(),"drawable",getPackageName());
+                System.out.println(id);
+
+                image.setImageResource(id);
+
+                mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+              //  int id = getResources().getIdentifier(flagIso,"drawable",getPackageName());
+
+                int message =  getResources().getIdentifier(flagIso, "string", getPackageName());
+                mQuestionTextView.setText(message);
+
+            }
+
+
+        });
+
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,4 +164,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
